a0e497fc2ff21295794c5dd63e06d81195bca80e
Tim Savannah <kata198@gmail.com>
2017-05-13 16:56:44 -0400

    3.0.3 -- THE LAST ONE FOR NOW I SWEAR

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         extra_install_requires = []
 
     setup(name='NetFetch',
-            version='3.0.2',
+            version='3.0.3',
             packages=['NetFetch'],
             scripts=['netFetchPut', 'netFetchGet', 'netFetchDelete'],
             author='Tim Savannah',
